dd510de22eefc113e12365ee3fdbd0d0aebb8ef3
bhautik patel <55433482+bhautik963@users.noreply.github.com>
2020-10-24 09:03:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const app = require('./app');
+const port = process.env.PORT || 3800;
+const mongoDb = process.env.MONGODB_URL ;
+
+mongoose.connect('mongodb+srv://social:1234567890@cluster0.ks75u.mongodb.net/social?retryWrites=true&w=majority', { useNewUrlParser: true })
+.then(() => {
+    console.log('DB: Connect OK!');
+    app.listen(port, () => {
+        console.log('Server running on => http://localhost:' + port);
+    });
+})
+.catch(err => console.log(err));
+
+console.log('Starting...');
